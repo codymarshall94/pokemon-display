e8335a6dc7aa81511d144fbe6dcea9096a0bac60
Cody Marshall <codymstuntsx@gmail.com>
2022-06-21 16:34:38 -0700

    gave padding to the buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Pagination({goNextPage, goPrevPage}) {
   return (
-    <div>
+    <div className='p-3'>
         <button type='button' onClick={goPrevPage}>Previous</button>
         <button type='button' onClick={goNextPage}>Next</button>
     </div>
