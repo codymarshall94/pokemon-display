aff47edaabfd4f375979461ad3653c9822dad9e8
Cody Marshall <codymstuntsx@gmail.com>
2022-06-21 16:33:42 -0700

    refactored code and made things into their own comps

--- src/components/PokemonDescription.js
+++ src/components/PokemonDescription.js
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/pokemondescription.css";
+import CategorySelector from "./CategorySelector";
+import PokemonAbout from "./PokemonAbout";
+import PokemonEvolution from "./PokemonEvolution";
+import PokemonMoves from "./PokemonMoves";
+import PokemonStats from "./PokemonStats";
 
-const RenderPokemonDetails = ({ selectedPokemonDetails }) => {
+const RenderPokemonDetails = ({
+  selectedPokemonDetails,
+  category,
+  setCategory,
+}) => {
   if (selectedPokemonDetails.length !== 0) {
+    let typeStyle = selectedPokemonDetails.types[0].type.name;
+
     return (
       <>
-        <div className="container pokemon-container">
+        <div className={`container pokemon-container ${typeStyle}`}>
+          {/* Top Half of Pokemon Display Card */}
+
           <div className="pokemon-content">
-            <h1>{selectedPokemonDetails.name}</h1>
+            <h1>
+              {selectedPokemonDetails.name[0].toUpperCase() + selectedPokemonDetails.name.slice(1)}{" "}
+              <span>#{selectedPokemonDetails.id}</span>
+            </h1>
+          </div>
+          <div className="pokemon-content">
+            {selectedPokemonDetails.types.map((type, index) => {
+              return (
+                <span key={index} className="type-pill">
+                  {type.type.name}
+                </span>
+              );
+            })}
           </div>
           <div className="pokemon-content">
             <img
-              src={selectedPokemonDetails.sprites.other.home.front_default}
+              src={
+                selectedPokemonDetails.sprites.other["official-artwork"]
+                  .front_default
+              }
               className="pokemon-image"
+              alt="pokemon"
             />
           </div>
-          <div className="pokemon-content">
-              <h4>Type</h4>
-          </div>
-          <div className="pokemon-content">
-            <div>
-              <h4>Height</h4>
-              <span>{selectedPokemonDetails.height}</span>
-            </div>
-            <div>
-              <h4>Weight</h4>
-              <span>{selectedPokemonDetails.weight}</span>
-            </div>
+
+          {/* Bottom Half of Pokemon Display Card */}
+
+          <div className="pokemon-content bottom-half">
+            <CategorySelector setCategory={setCategory}/>
+
+            {category === "about" ? (
+              <PokemonAbout selectedPokemonDetails={selectedPokemonDetails} />
+            ) : null}
+            {category === "stats" ? (
+              <PokemonStats selectedPokemonDetails={selectedPokemonDetails} />
+            ) : null}
+            {category === "evolution" ? (
+              <PokemonEvolution selectedPokemonDetails={selectedPokemonDetails} />
+            ) : null}
+            {category === "moves" ? (
+              <PokemonMoves selectedPokemonDetails={selectedPokemonDetails} />
+            ) : null}
           </div>
         </div>
       </>
@@ -37,9 +72,15 @@ const RenderPokemonDetails = ({ selectedPokemonDetails }) => {
 };
 
 function PokemonDescription({ selectedPokemonDetails }) {
+  const [category, setCategory] = useState("about");
+
   return (
     <>
-      <RenderPokemonDetails selectedPokemonDetails={selectedPokemonDetails} />
+      <RenderPokemonDetails
+        selectedPokemonDetails={selectedPokemonDetails}
+        category={category}
+        setCategory={setCategory}
+      />
     </>
   );
 }
