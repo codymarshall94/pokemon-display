943517b71b1c776e5dabafc8cd6acf40d5f56ae0
Cody Marshall <codymstuntsx@gmail.com>
2022-06-23 15:47:44 -0700

    added loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/loading.css';
+
+function Loading() {
+  return (
+      <div class="container">
+        <div class="ball">
+          <div class="ball-btn">
+            <div class="ball-btn-ring"></div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default Loading;
