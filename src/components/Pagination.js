90396e9fe7c1431c1143ea8536b9b75c08fef546
Cody Marshall <codymstuntsx@gmail.com>
2022-06-27 10:55:26 -0700

    added chevron icon

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Pagination({goNextPage, goPrevPage, prevPage, nextPage}) {
   return (
     <div className='p-3'>
-        {prevPage && <button type='button' onClick={goPrevPage}>Previous</button>}
-        {nextPage && <button type='button' onClick={goNextPage}>Next</button>}
+        {prevPage && <button type='button' onClick={goPrevPage}><FaChevronLeft /></button>}
+        {nextPage && <button type='button' onClick={goNextPage}><FaChevronRight /></button>}
     </div>
   )
 }
