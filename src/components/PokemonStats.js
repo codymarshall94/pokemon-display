6eb3590bb6d5dc556c04e26072e129f282b1088b
Cody Marshall <codymstuntsx@gmail.com>
2022-06-27 10:57:14 -0700

    added new styling boundaries

--- src/components/PokemonStats.js
+++ src/components/PokemonStats.js
@@ -2,16 +2,18 @@ import React from "react";
 import "../css/pokemonstats.css";
 
 function PokemonStats({ selectedPokemonDetails }) {
+  let typeStyleOne = selectedPokemonDetails.types[0].type.name;
+
   return (
     <div className="container stats-container">
       <h3 className="text-center">Base Stats</h3>
-      {selectedPokemonDetails.stats.map((stat) => (
-        <div className="row d-flex justify-content-center">
+      {selectedPokemonDetails.stats.map((stat, index) => (
+        <div key={index} className="row d-flex justify-content-center">
           <h4 className="col">{stat.stat.name}</h4>
           <span className="col">{stat["base_stat"]}</span>
           <div className="progress p-0">
             <div
-              className="progress-bar"
+              className={`progress-bar ${typeStyleOne}`}
               role="progressbar"
               style={{width: Math.floor((stat["base_stat"] / 255 * 100)) + "%"}}
               aria-valuenow="25"
