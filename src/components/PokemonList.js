86f7beb1e53a9c2f2dbbc0907cedfae3eb9d392b
Cody Marshall <codymstuntsx@gmail.com>
2022-06-27 10:56:49 -0700

    changed algorithm for rending list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,20 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "../css/pokemonlist.css";
+import Loading from "./Loading";
 
-function PokemonList({ pokemon, handleSelectedPokemon}) {
-  return (
-    <div className="container">
-      {pokemon.map((poke, index) => (
-        <div
-          className="list-style"
-          key={index}
-          onClick={() => handleSelectedPokemon(poke)}
-        >
-          <span className="px-2">{poke[0].toUpperCase() + poke.slice(1)}</span>
+const RenderList = ({ pokemonList, handleSelectedPokemon }) => {
+  if (pokemonList) {
+    return (
+      <div className="pokemon-list">
+        {pokemonList.map((poke, index) => (
+          <div
+            className="list-style"
+            key={index}
+            onClick={() => handleSelectedPokemon(poke.name)}
+          >
+            <img src={poke.image} className="list-image" alt="pokemon" />
+            {poke.id}
+            {poke.name}
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+};
+
+function PokemonList({ handleSelectedPokemon, currentPage }) {
+  const [pokemonList, setPokemonList] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let currentPageInfo = [];
+    let pokeInfo = [];
+    setLoading(true);
+
+    const listPokemon = async () => {
+      const req = await axios.get(currentPage);
+      const res = await req.data.results;
+      currentPageInfo.push(res);
+      console.log(currentPageInfo);
+
+      for (let i = 0; i <= res.length - 1; i++) {
+        let req = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${currentPageInfo[0][i].name}`
+        );
+        let res = await req.data;
+        pokeInfo.push(res);
+      }
+
+      const pokemon = pokeInfo.map((poke) => ({
+        name: poke.name,
+        id: poke.id,
+        image: poke.sprites["front_default"],
+      }));
+
+      setPokemonList(pokemon);
+      setLoading(false);
+    };
+
+    listPokemon();
+  }, [currentPage]);
+
+  if (loading) {
+    return (
+      <div className="container">
+        <div>
+          <h2 className="list-header">Choose a Pokemon to view information</h2>
         </div>
-      ))}
-    </div>
-  );
+        <Loading />
+      </div>
+    );
+  } else {
+    return (
+      <div className="container d-flex flex-column justify-content-center">
+        <div>
+          <h2 className="list-header">Choose a Pokemon to view information</h2>
+        </div>
+        <RenderList
+          pokemonList={pokemonList}
+          handleSelectedPokemon={handleSelectedPokemon}
+        />
+      </div>
+    );
+  }
 }
 
 export default PokemonList;
